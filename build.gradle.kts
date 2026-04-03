fd1f85c9258dd4f9082992fbbcb26dab67f461d8
Zeyu Yang <123612557+zeyu-yang-dev@users.noreply.github.com>
2026-04-03 18:33:56 +0200

    refactor: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
 }
 
-group = "io.github.zeyu.staircasecardgame"
+group = "io.github.zeyuyangdev.staircasecardgame"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -23,6 +23,8 @@ kotlin {
 }
 
 application {
+    mainClass.set("io.github.zeyuyangdev.sixcardgolf.MainKt")
+
     applicationDefaultJvmArgs = listOf(
         "--add-opens", "java.desktop/sun.awt=ALL-UNNAMED",
         "--add-opens", "java.desktop/java.awt.peer=ALL-UNNAMED",
