75f7f8364d685065fe5c2fccbeaa82dfcdd38bab
Zeyu Yang <123612557+zeyu-yang-dev@users.noreply.github.com>
2026-04-08 22:30:37 +0200

    docs: standardize comments in entity layer

--- src/main/kotlin/io/github/zeyuyangdev/cardstaircase/entity/CardStaircase.kt
+++ src/main/kotlin/io/github/zeyuyangdev/cardstaircase/entity/CardStaircase.kt
@@ -6,21 +6,18 @@ import java.util.Stack
  * Root entity that holds the whole game state.
  *
  * @property players        the two players in turn order (size = 2)
- * @property stairs         the 5 stair stacks; each row is a [Stack] of [Card]
- * @property currentPlayer  index of the player whose turn it is (0 or 1)
- * @property gameLog        list of public log messages (anonymized)
+ * @property stairs         a list of 5 stacks representing the staircase
+ * @property currentPlayer  index of the player who's in turn (0 or 1)
+ * @property gameLog        list of log messages
  * @property stairsModified flag set to true once a stair was changed during the current turn
  * @property drawStack      the stack representing the draw stack
  * @property discardStack   the stack representing the discard stack
  */
 data class CardStaircase(val players: List<Player>) {
     var currentPlayer: Int = 0
-
     var stairs: List<Stack<Card>> = listOf(Stack(), Stack(), Stack(), Stack(), Stack())
     var stairsModified: Boolean = false
-
     var gameLog: MutableList<String> = mutableListOf()
-
     var drawStack: Stack<Card> = Stack()
     var discardStack: Stack<Card> = Stack()
 }
